9ad1eceaceb8fbc8a9c275dbb74c879646496308
Zicozydasilva <Zicozydasilva70@gmail.com>
2020-06-29 13:21:03 +0100

    Updates

---
+++ precache-manifest.174bdca59803a8b30cb91000c8720429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf4ce267ce4f2fb674fc642cb2134ef",
+    "url": "/budgetcalc/index.html"
+  },
+  {
+    "revision": "1c4a74b329de9ecd1a4b",
+    "url": "/budgetcalc/static/css/main.f58b4dd9.chunk.css"
+  },
+  {
+    "revision": "e28ab4bfddecdc7f10bb",
+    "url": "/budgetcalc/static/js/2.fcef04b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budgetcalc/static/js/2.fcef04b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4a74b329de9ecd1a4b",
+    "url": "/budgetcalc/static/js/main.ea45ad1c.chunk.js"
+  },
+  {
+    "revision": "10cf289003ac908d96c3",
+    "url": "/budgetcalc/static/js/runtime-main.2cc0e9ce.js"
+  }
+]);
